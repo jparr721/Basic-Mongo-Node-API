5fbe6500f0b51c49a03325e221b8d4c4ef3462ed
Jarred Parr <jparr721@gmail.com>
2017-08-14 16:12:10 -0400

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+require('./config/config');
+
+const _ = require('lodash');
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const {
   ObjectID
 } = require('mongodb');
@@ -11,20 +14,21 @@ var {
   Todo
 } = require('./models/todo');
 var {
-  Users
+  User
 } = require('./models/user');
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text: req.body.text,
+    text: req.body.text
   });
 
-  todo.save().then((todo) => {
-    res.send(todo)
+  todo.save().then((doc) => {
+    res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
@@ -37,14 +41,14 @@ app.get('/todos', (req, res) => {
     });
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
@@ -60,10 +64,75 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Started at port ${PORT}`);
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {
-  app,
+  app
 };
